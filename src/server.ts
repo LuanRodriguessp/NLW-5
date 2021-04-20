1e61bae10c7bb3dc618cde439278700f9e96cc28
Luan Rodrigues <luan.rodrigues.souza@icloud.com>
2021-04-20 14:03:04 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from "express";
+
+
+const app = express();
+
+
+/**
+ * GET = Buscas 
+ * POST = Criação
+ * PUT = Alteração
+ * DELETE = Deletar
+ * PATH = Alterar uma informação especifica
+ */
+
+app.get("/", (request, response)  => {
+    return response.json({
+        message: "Ola NLW 05",
+    })
+})
+
+app.post("/", (request, response) => {
+    return response.json({
+         message: "Usuario salvo com sucesso !"});
+})
+
+
+app.listen(4545, () => console.log("Server is running on port 4545"));
